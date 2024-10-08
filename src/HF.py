438b3f40bd53f14afd73173910f69ce734bdfe2a
William <131210083+feralhotdog@users.noreply.github.com>
2024-10-08 16:03:09 -0500

    commit

--- src/HF.py
+++ src/HF.py
@@ -1,5 +1,22 @@
-#implement Hartree-Fock Calculations here
+#automatically implement Hartree-Fock Calculations for given input file
+#Format: python3 /path/to/HF.py /path/to/input/file/
 
 import psi4
+import os
+import sys
 
 
+#Setting output file name based on input file name.
+input_file = sys.argv[1]
+base=os.path.basename(sys.argv[1])
+mol_name=os.path.splitext(base)[0]
+out_file_name=mol_name + ".dat"
+psi4.core.set_output_file(os.path.join("/home/wh/PhD/FA24/CHEM6280/MP2/Data/results", out_file_name), False)
+
+psi4.set_memory('2 GB')
+
+contents=open(sys.argv[1])
+
+moleucle = psi4.geometry(f"@{input_file}")
+
+psi4.energy('scf/cc-pvdz')
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,22 @@
 #helper functionss
+
+import argparse
+
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        # Process the file content here
+        print(content)
+
+if __name__ == "__main__":
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Read and process a file.")
+    
+    # Add a positional argument for the file path
+    parser.add_argument("file", help="Path to the file to be processed")
+    
+    # Parse the command line arguments
+    args = parser.parse_args()
+    
+    # Call the file processing function with the file path
+    process_file(args.file)
